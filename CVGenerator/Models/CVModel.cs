dde49f8baa55defc6b302935ec9f6d673770280c
George Ngigi <ngigegeorge023@gmail.com>
2024-11-21 05:06:45 +0300

    Form validation

--- CVGenerator/Models/CVModel.cs
+++ CVGenerator/Models/CVModel.cs
@@ -5,15 +5,15 @@ public class CVModel
     This model is used to store the data from the form.
     It is important that the variable name to match the name attribute in the form.
     */
-    public string Name { get; set; }
-    public string Phone { get; set; }
-    public string Email { get; set; }
-    public string Address { get; set; }
+    public string Name { get; set; } = "";
+    public string Phone { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string Address { get; set; } = "";
 
     // raw string
-    public string ProfessionalSummary { get; set; }
-    public string WorkExperience { get; set; }
-    public string EducationBackground { get; set; }
-    public string Skills { get; set; }
-    public string Languages { get; set; }
+    public string ProfessionalSummary { get; set; } = "";
+    public string WorkExperience { get; set; } = "";
+    public string EducationBackground { get; set; } = "";
+    public string Skills { get; set; } = "";
+    public string Languages { get; set; } = "";
 }
